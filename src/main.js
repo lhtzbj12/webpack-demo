46283345f662dc86de203affb042335d22f4734b
lhtzbj12 <lhtzbj12@126.com>
2018-01-28 18:04:56 +0800

    webpack基本配置，支持将ES6+转成ES5;支持将css、scss、less文件编译找包

---
+++ src/main.js
@@ -0,0 +1,19 @@
+﻿import "./style1.css"
+import "./style2.scss"
+import "./style3.less"
+//let测试
+let a = 1
+{
+    let a=2
+}
+console.log(a)
+//()=>测试
+let b = [1, 2, 3, 4]
+b.map((x)=>{
+    console.log(x)
+})
+//换行
+let msg=`这是一段非常非常长的换行文字，ES6之前是用单引号和\，
+现在可以直接用\`,很方便
+是吧！`
+console.log(msg)
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, 'src/main.js'),       //入口
+    },
+    output: {
+        filename: 'bundle.js',               //输出的文件名
+        path: path.resolve(__dirname, 'build') //输出文件所在的目录
+    },
+    devServer: { // 检测代码变化并自动重新编译并自动刷新浏览器
+        contentBase: path.resolve(__dirname, 'build') // 设置静态资源的根目录
+    },
+    module: { // 如何处理项目中不同类型的模块
+        rules: [ // 用于规定在不同模块被创建时如何处理模块的规则数组
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "env", "react"
+                        ]
+                    }
+                },
+                exclude: path.resolve(__dirname, 'node_modules'),
+                include: path.resolve(__dirname, 'src'),
+            },
+            {
+                test: /\.css$/,
+                use:ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:"css-loader"
+                })
+            },
+            {
+                test: /\.scss$/,
+                use:ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:[{
+                        loader:"css-loader"
+                    },{
+                        loader:"sass-loader"
+                    }]
+                })
+            },
+            {
+                test: /\.less$/,
+                use:ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:[{
+                        loader:"css-loader"
+                    },{
+                        loader:"less-loader"
+                    }]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('css/index.css') //css scss less文件打包生成单个的css文件
+    ]
+}
